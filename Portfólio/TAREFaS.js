012c0c0aa51a999a5be49cd30e5b510b1425270c
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2023-10-25 12:05:30 -0300

    Chosee: my initial commit

---
+++ Portfólio/TAREFaS.js
@@ -0,0 +1,47 @@
+/*
+MENU LATERAL ESQUERDO:
+- titulo Projetos
+- inicio: tela de inicio será tela de destaque com meu melhor conteúdo ou melhores
+- Mobile
+- PC
+- Multiplayer
+
+SOBRE LATERAL DIREITO:
+- foto
+- tecnologias abaixo da foto
+- redes sociais
+- contact me
+- About Me / Sobre mim
+
+CONTEUDO:
+- jogos
+- nome
+- link
+- fotos
+- video
+- resumo explicativo
+
+
+
+
+
+
+    <section class="flex" id="about"><!--BARRA LATERAL ESQUERDO SOBRE-->
+        <div id="interfaceaboutme">
+            <nav>
+                <h1 id="myname">Jorge <span>Mauadie</span></h1>
+                <img id="profilephoto" src="Imagens/Foto Perfil/photoprofile.png" alt="My Profile Picture">
+                <ul>
+                    <!--por imagens aqui -->
+                    <li><a href="aqui vai o link da pagina incial"></a>Linkedin</li>
+                    <li><a href="aqui vai o link da pagina incial"></a>GitHub</li>
+                    <!--Aqui vai a lateral esquerda com oss tipos de conteudo (inicio, mobile, pc, multiplayer)-->
+                </ul>
+            </nav>
+        </div>
+        <!--menu lateral direito-->
+    </section> 
+
+
+
+*/
