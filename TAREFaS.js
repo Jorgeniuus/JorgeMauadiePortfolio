de0f689d5a6fc68d917d3fd3b78d61366355f135
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2023-10-26 22:13:35 -0300

    chore:
    - update

--- TAREFaS.js
+++ TAREFaS.js
@@ -16,47 +16,11 @@ SOBRE LATERAL DIREITO:
 CONTEUDO:
 - jogos
 - nome
-- link = fazer o link ser no nome do jogo
 - fotos
 - video
 - resumo explicativo
 
 TAREFA:
-- Por fotos de amostraa dentro de um coteiner e deixa-las menor,
-    quando passar o mouse por cima elas crescerem.
-
-======================================================================================================
-CONTEUDOS DO A BEEAST OUTSIDE:
-- link game: https://gamedevbyjorge.itch.io/a-beast-outside
-- link youtube: https://youtu.be/-j-JZupnu0U
-- link youtube iframe: <iframe width="347" height="195" src="https://www.youtube.com/embed/-j-JZupnu0U" title="A Beast Outside - Trailer!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-- descrição En: A Beast Outside is a game was done for a Game Jam, with theme of game Horror and Feral Beast. It was my first Game Jam Game. 
-- descrição Pt: A Beast Outside é um jogo que foi feito para uma Game Jam, cujo o tema era Horror and Feral Beast. Esse foi meu primeiro jogo para uma Game Jam!   
-
-CONTEUDOS DO FPS MULTIPLAYER:
-- link game: https://gamedevbyjorge.itch.io/fps-multiplayer
-- descrição En: A shooter multiplayer game made with Photon Pun for the purpose of learning the tool!
-- descrição Pt: Um jogo multijogador de tiro feito com Photon Pun com o propósito de aprender a ferramenta!
-
-ABOUT ME:
-- GitHub: https://github.com/Jorgeniuus
-- Linkedin: https://www.linkedin.com/in/jorgemauadie/
-
-======================================================================================================
-    <section class="flex" id="about"><!--BARRA LATERAL ESQUERDO SOBRE-->
-        <div id="interfaceaboutme">
-            <nav>
-                <h1 id="myname">Jorge <span>Mauadie</span></h1>
-                <img id="profilephoto" src="Imagens/Foto Perfil/photoprofile.png" alt="My Profile Picture">
-                <ul>
-                    <!--por imagens aqui -->
-                    <li><a href="aqui vai o link da pagina incial"></a>Linkedin</li>
-                    <li><a href="aqui vai o link da pagina incial"></a>GitHub</li>
-                    <!--Aqui vai a lateral esquerda com oss tipos de conteudo (inicio, mobile, pc, multiplayer)-->
-                </ul>
-            </nav>
-        </div>
-        <!--menu lateral direito-->
-    </section> 
-
+- 
+=====================================================================================================
 */
