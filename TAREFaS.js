7e5cd3bb791f4fddebe1497b66b73f540fc8c004
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2023-10-28 14:10:18 -0300

    chore:
    -Added Resume in english only by now

--- TAREFaS.js
+++ TAREFaS.js
@@ -22,5 +22,23 @@ CONTEUDO:
 
 TAREFA:
 - 
+SOBRRE MIM
+- Português:
+    Sou um desenvolvedor de jogos que utiliza a Game Engine Unity 3D, que busca 
+sempre melhorar os conhecimentos na área, sendo autodidata, estudando e 
+aprendendo por conta própria a fim de melhorar minhas habilidades de 
+desenvolvimento e programação. No momento estou aprendendo a desenvolver jogos
+multiplayer usando Photon Pun e Mirror Networking. No futuro pretendo começar a 
+aprender desenvolvimento de jogos em VR e AR para jogos em Unity. Recentemente 
+tenho aprendido Front-End para construir meu próprio portfólio.
+
+- Inglês:
+    I am a game developer who utilizes Unity 3D Game Engine and constantly 
+strives to enhance my knowledge in the field. I am a self-taught individual, dedicated 
+to studying and learning independently to improve my development and programming 
+skills. In the moment I am learning to develop multiplayer games usign Photon Pun and 
+Mirror Networking. In the future I intend starting learn VR and AR development for 
+games in unity. Recently I have been learning front-end to build my own portfolio. 
+
 =====================================================================================================
 */
