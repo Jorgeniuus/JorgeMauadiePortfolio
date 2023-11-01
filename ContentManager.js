d5be8ac4ae0b4b53f6e5d31175e79f732ba922e7
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2023-11-01 15:39:04 -0300

    test

--- ContentManager.js
+++ ContentManager.js
@@ -137,3 +137,89 @@ function TecnologiesTimeExperience(whichTecnologyIs){
     }
 }
 // TecnologiesTimeExperience(typeTecnology[0])
+//===================================== TRANSLATE ========================
+//menu
+let projects = document.querySelector('h1#titleprojects'); 
+let home = document.querySelector('li#home');
+let multiplayer = document.querySelector('li#multiplayer');
+let aboutme = document.querySelector('li#aboutme');
+// //resume
+let resume = document.querySelector('.resume');
+//game descriptions
+let iframeContentGames = document.getElementById('iframeContentGame');
+// //tecnology descrriptions
+// let udityDescription = document.querySelector(''); //estao em falta
+// let frontEndDescription = document.querySelector('');
+
+let currentTranslate;
+let nameGameContentIframe;
+
+let flagTranaslateElements = document.querySelectorAll('.translateElement');
+
+$(".translateElement").on('touchstart click', function(event) {
+    if(event.type == "click"){
+        flagTranaslateElements.forEach(element => {
+            let value = this.getAttribute('data-value');
+            if(element.getAttribute('data-value') == value){
+                TranslateConversion(value);
+            }
+        });
+    }else if(event.type == "touchstart"){
+        flagTranaslateElements.forEach(element => {
+            let value = this.getAttribute('data-value');
+            if(element.getAttribute('data-value') == value){
+                TranslateConversion(value);
+            }
+        });
+    }
+});
+
+// let flagTranaslateElements = document.querySelectorAll('.translateElement');
+// flagTranaslateElements.forEach(element => {
+//     element.addEventListener('touchstart', function() {
+//         let value = this.getAttribute('data-value');
+//         TranslateConversion(value);
+//     });
+// });
+
+TranslateConversion()
+function TranslateConversion(idLanguage = 0){ 
+
+    fetch('TranslateData.json')
+    .then(response => response.json())
+    .then(data => {
+        //'data' representa o JSON convertido em JavaScript
+        Translation(data[idLanguage]) 
+    })
+    .catch(error => {
+        // Tratamento de erro, caso a requisição falhe
+        console.error('Erro ao buscar o arquivo JSON:', error);
+    });
+}
+function Translation(languageSelected){
+    projects.innerHTML = languageSelected.projects
+    home.innerHTML = languageSelected.home
+    multiplayer.innerHTML = languageSelected.multiplayer
+    aboutme.innerHTML = languageSelected.aboutme
+
+    resume.innerHTML = languageSelected.resume
+
+    currentTranslate = languageSelected
+
+    // setInterval(TranslateGameContentsInIframes, 3000);
+}
+// function TranslateGameContentsInIframes(whichGameContentIframeIs = nameGameContentIframe){
+//     let innerDoc = iframeContentGames.contentDocument || iframeContentGames.contentWindow.document;
+//     let innertext = innerDoc.querySelector('body section div p')
+
+//     nameGameContentIframe = whichGameContentIframeIs
+
+//     if(whichGameContentIframeIs == "shipdestroyer"){
+//         innertext.innerHTML = currentTranslate.descriptionShipDestroyer
+//     }
+//     else if(whichGameContentIframeIs == "abeastoutside"){
+//         innertext.innerHTML = currentTranslate.descriptionABeastOutside
+//     }else{
+//         innertext.innerHTML = currentTranslate.descriptionFPSMultiplayer
+//     }
+// }
